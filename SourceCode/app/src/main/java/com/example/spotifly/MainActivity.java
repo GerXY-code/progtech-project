6e4c4453427556ab7921baa2b9b16b6928d2338a
Gergő Péter Lehoczki <gerixy20@gmail.com>
2023-05-17 18:34:55 +0200

    Database connection created

--- SourceCode/app/src/main/java/com/example/spotifly/MainActivity.java
+++ SourceCode/app/src/main/java/com/example/spotifly/MainActivity.java
@@ -3,19 +3,43 @@ package com.example.spotifly;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
 
+public class MainActivity extends AppCompatActivity {
+    TextView text,errorText;
+
+    Button show;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         goToRegisterPage();
+        text = (TextView) findViewById(R.id.appName_txt2);
 
+
+        show = (Button) findViewById(R.id.login_btn);
+
+        show.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View view) {
+
+                new Async().execute();
+
+            }
+
+        });
     }
 
     /*
@@ -33,6 +57,67 @@ public class MainActivity extends AppCompatActivity {
             }
         });
     }
+    class Async extends AsyncTask<Void, Void, Void> {
+
+
+
+        String records = "",error="";
+
+        @Override
+
+        protected Void doInBackground(Void... voids) {
+
+            try
+
+            {
+
+                Class.forName("com.mysql.jdbc.Driver");
+                Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.0.206/test", "root", "");
+                Statement statement = connection.createStatement();
+                ResultSet resultSet = statement.executeQuery("SELECT * FROM test.test");
+                while(resultSet.next()) {
+
+                    records += resultSet.getString(1) + " " + resultSet.getString(2) + "\n";
+
+                }
+
+            }
+
+            catch(Exception e)
+
+            {
+
+                error = e.toString();
+                Log.i("errr", error);
+
+            }
+
+            return null;
+
+        }
+
+
+
+        @Override
+
+        protected void onPostExecute(Void aVoid) {
+
+            text.setText(records);
+
+            if(error != "")
+
+                errorText.setText(error);
+
+            super.onPostExecute(aVoid);
+
+        }
+
+
+
+
+
+    }
+
 
 
 
