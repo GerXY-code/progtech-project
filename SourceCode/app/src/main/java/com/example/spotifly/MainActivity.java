c568fc62a79306d6a9d57b26c46f367e52b2f363
hDani03 <danielhuszka1@gmail.com>
2023-05-18 21:38:11 +0200

    Login update

--- SourceCode/app/src/main/java/com/example/spotifly/MainActivity.java
+++ SourceCode/app/src/main/java/com/example/spotifly/MainActivity.java
@@ -19,7 +19,7 @@ import java.sql.Statement;
 public class MainActivity extends AppCompatActivity {
     TextView text,errorText;
     EditText UserName, Password;
-    public static String username, password, convertedHashedPassword;
+    public static String username,password, convertedHashedPassword;
 
 
     Button show;
@@ -28,13 +28,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         goToRegisterPage();
+
+        UserName = (EditText)findViewById(R.id.username_input_txt);
+        Password = (EditText)findViewById(R.id.password_input_txt);
+
         login();
 
         text = (TextView) findViewById(R.id.appName_txt2);
         show = (Button) findViewById(R.id.login_btn);
 
-        UserName = (EditText) findViewById(R.id.username_input_txt);
-        Password = (EditText) findViewById(R.id.password_input_txt);
+
 
         show.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -62,67 +65,66 @@ public class MainActivity extends AppCompatActivity {
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                username = UserName.getText().toString();
+                //username = UserName.getText().toString();
+                username = "dddddd";
                 password = Password.getText().toString();
+                Log.d("nev:", username);
                 Integer hashedPassword = password.hashCode();
                 convertedHashedPassword = hashedPassword.toString();
                 new Async().execute();
             }
         });
     }
-    class Async extends AsyncTask<String,String, String> {
-        String records = "",error="";
 
-        @Override
-        protected String doInBackground(String... strings) {
-            try
-            {
-                Class.forName("com.mysql.jdbc.Driver");
-                Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.0.206:3306/spotifly", "root", "");
-                Statement statement = connection.createStatement();
-                ResultSet resultSet = statement.executeQuery(
-                        "SELECT username, password FROM spotifly.users WHERE username='dani'");
-                while(resultSet.next()) {
-                    records += resultSet.getString(1) + " " + resultSet.getString(2) + "\n";
-                }
-                if (records==""){
-                    Log.d("hiba", "Sikertelen Login");
-                    Log.d("rekordok:", records);
-                }
-                else{
-                    Log.d("siker","Sikeres login");
-                    Log.d("rekordok:", records);
-                    Log.d("fnev::", username);
-                }
-            }
+}
+class Async extends AsyncTask<String,String, String> {
+    String username=MainActivity.username;
+    String records = "",error="";
 
-            catch(Exception e)
-            {
-                error = e.toString();
-                Log.i("errr", error);
+    @Override
+    protected String doInBackground(String... strings) {
+        try
+        {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.0.206:3306/spotifly", "root", "");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery(
+                    "SELECT username, password FROM spotifly.users WHERE username='dani'");
+            while(resultSet.next()) {
+                records += resultSet.getString(1) + " " + resultSet.getString(2) + "\n";
             }
-            return null;
+            if (records==""){
+                Log.d("hiba", "Sikertelen Login");
+                Log.d("rekordok:", records);
+            }
+            else{
+                Log.d("siker","Sikeres login");
+                Log.d("rekordok:", records);
+                Log.d("fnev:", username);
+            }
         }
 
-
-
-        @Override
-        protected void onPostExecute(String st) {
-
-            text.setText(records);
-
-            if(error != "")
-
-                errorText.setText(error);
-
-            super.onPostExecute(st);
-
+        catch(Exception e)
+        {
+            error = e.toString();
+            Log.i("errr", error);
         }
-
-
+        return null;
     }
 
 
 
+    @Override
+    protected void onPostExecute(String st) {
 
+        /*
+        text.setText(records);
+
+        if(error != "")
+
+            errorText.setText(error);
+
+        super.onPostExecute(st);
+        */
+    }
 }
