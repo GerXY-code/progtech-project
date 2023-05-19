a4ea95f84c7638db34221b315b57d51054c117ca
Gergő Péter Lehoczki <gerixy20@gmail.com>
2023-05-19 22:33:19 +0200

    Get music attrs from AsyncTask

---
+++ SourceCode/app/src/main/java/com/example/spotifly/AsyncGetMusics.java
@@ -0,0 +1,59 @@
+package com.example.spotifly;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import java.sql.Array;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+public class AsyncGetMusics extends AsyncTask<ArrayList<String[]>, ArrayList<String[]>, ArrayList<String[]>> {
+
+
+    String username = RegisterActivity.usernameVerified;
+    String email = RegisterActivity.emailVerified;
+    String password = RegisterActivity.hashedPassword;
+    String records = "", error = "";
+    String musicName,author,duration;
+    String [] musicAttrs = new String[3];
+
+    ArrayList<String[]> musicList = new ArrayList<>();
+
+
+
+
+
+    @Override
+    protected ArrayList<String[]> doInBackground(ArrayList<String[]>... arrayLists) {
+        try {
+
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.0.206/spotifly", "root", "");
+            Statement statement = connection.createStatement();
+            ResultSet resultSet = statement.executeQuery("SELECT * FROM musics");
+            while(resultSet.next()) {
+                //records += resultSet.getString(1) + " " + resultSet.getString(2) + " " + resultSet.getTime(3) + "\n";
+                musicName = resultSet.getString(2);
+                author    = resultSet.getString(3);
+                duration  = resultSet.getString(4);
+                musicAttrs[0] = musicName;
+                musicAttrs[1] = author;
+                musicAttrs[2] = duration;
+                musicList.add(musicAttrs);
+            }
+
+
+        } catch (Exception e) {
+
+            error = e.toString();
+            Log.i("errr", error);
+
+        }
+
+        return musicList;
+    }
+}
--- SourceCode/app/src/main/java/com/example/spotifly/HomePageActivity.java
+++ SourceCode/app/src/main/java/com/example/spotifly/HomePageActivity.java
@@ -3,11 +3,21 @@ package com.example.spotifly;
 import android.content.Intent;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ExecutionException;
 
 
 public class HomePageActivity extends AppCompatActivity {
@@ -17,15 +27,39 @@ public class HomePageActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
-
         Playlist p = new Playlist();
         PlaylistCommand addToPlaylistCommand      = new PlaylistCommand(p,Action.AddNewMusic,"armanen");
         PlaylistCommand removeFromPlaylistCommand = new PlaylistCommand(p,Action.RemoveMusicFrom, "armanen");
         addToPlaylistCommand.call();
         removeFromPlaylistCommand.call();
 
+        getMusics();
+
 
     }
+
+
+    protected void getMusics(){
+
+        ArrayList<String[]> musics = new ArrayList<>();
+        AsyncGetMusics task = new AsyncGetMusics();
+        try {
+            musics = task.execute().get();
+            for (int i = 0; i < musics.size(); i++) {
+
+
+            }
+            Log.d("Cim", musics.get(0)[0]);
+            Log.d("Szerző", musics.get(0)[1]);
+            Log.d("Időtartam", musics.get(0)[2]);
+
+        } catch (ExecutionException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 
 
 
