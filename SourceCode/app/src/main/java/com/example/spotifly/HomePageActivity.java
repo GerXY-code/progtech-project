cae486ad5c23fd126cde771151ecd8f204fb273d
Gergő Péter Lehoczki <gerixy20@gmail.com>
2023-05-19 14:34:26 +0200

    Music player testing
    I have tested, how to start a music in java

--- SourceCode/app/src/main/java/com/example/spotifly/HomePageActivity.java
+++ SourceCode/app/src/main/java/com/example/spotifly/HomePageActivity.java
@@ -1,14 +1,38 @@
 package com.example.spotifly;
 
+import android.content.Intent;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
 public class HomePageActivity extends AppCompatActivity {
+
+    MediaPlayer mp;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_page);
+        testForMusicPlaying();
+    }
+
+
+    public void testForMusicPlaying(){
+        Button play = (Button)findViewById(R.id.play_btn);
+
+        mp = MediaPlayer.create(this, R.raw.armanen);
+
+        play.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mp.start();
+                
+            }
+        });
     }
 }
