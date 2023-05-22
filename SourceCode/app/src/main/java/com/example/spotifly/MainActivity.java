aab59eb2232d826016786fbc2039541bdef9c03a
Gergő Péter Lehoczki <gerixy20@gmail.com>
2023-05-22 20:26:44 +0200

    re-clone
    Re-cloned the project

--- SourceCode/app/src/main/java/com/example/spotifly/MainActivity.java
+++ SourceCode/app/src/main/java/com/example/spotifly/MainActivity.java
@@ -88,18 +88,12 @@ class Async extends AsyncTask<Boolean,Boolean, Boolean> {
 
     @Override
     protected Boolean doInBackground(Boolean... Boolean) {
-        String ip;
-        try {
-          ip =  InetAddress.getHostAddress();
-          Log.d("asd", ip);
-        } catch (UnknownHostException e) {
-            throw new RuntimeException(e);
-        }
+
 
         try
         {
             Class.forName("com.mysql.jdbc.Driver");
-            Connection connection = DriverManager.getConnection("jdbc:mysql://"+ip+"/spotifly", "root", "");
+            Connection connection = DriverManager.getConnection("jdbc:mysql://192.168.0.45/spotifly", "root", "");
             Statement statement = connection.createStatement();
             ResultSet resultSet = statement.executeQuery(
                     "SELECT username, password FROM spotifly.users WHERE username=('"+username+"') AND password=('"+password+"')");
