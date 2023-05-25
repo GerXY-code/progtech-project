ff6fd17c357de983305c4d0b98d15dc56997466b
Gergő Péter Lehoczki <gerixy20@gmail.com>
2023-05-25 23:44:11 +0200

    Improved the database

--- Database/spotifly.sql
+++ Database/spotifly.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 17, 2023 at 12:55 PM
+-- Generation Time: May 25, 2023 at 11:43 PM
 -- Server version: 10.4.27-MariaDB
 -- PHP Version: 8.2.0
 
@@ -29,6 +29,7 @@ SET time_zone = "+00:00";
 
 CREATE TABLE `musics` (
   `id` int(11) NOT NULL,
+  `raw_id` varchar(255) DEFAULT NULL,
   `title` varchar(255) DEFAULT NULL,
   `author` varchar(255) DEFAULT NULL,
   `length` time DEFAULT NULL,
@@ -36,13 +37,29 @@ CREATE TABLE `musics` (
   `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- Dumping data for table `musics`
+--
+
+INSERT INTO `musics` (`id`, `raw_id`, `title`, `author`, `length`, `created_at`, `updated_at`) VALUES
+(1, 'one', 'Armanen', 'Patlamaya Devam', '00:02:42', '2023-05-19 22:05:54', '2023-05-19 22:05:54'),
+(2, 'two', 'Read all about it', 'Emilie Sandé', '00:04:10', '2023-05-21 21:17:54', '2023-05-21 21:17:54'),
+(3, 'three', 'Can\'t hold us', 'Macklemore & Ryan Lewis', '00:04:51', '2023-05-19 22:38:58', '2023-05-19 22:38:58'),
+(4, 'four', 'Mockingbird', 'Eminem', '00:04:00', '2023-05-25 21:15:43', '2023-05-25 21:15:43'),
+(5, 'five', 'Leave a light on', 'Tom Walker', '00:03:50', '2023-05-25 21:16:19', '2023-05-25 21:16:19'),
+(6, 'six', 'Someone you loved', 'Lewis Capaldi', '00:04:20', '2023-05-25 21:17:04', '2023-05-25 21:17:04'),
+(7, 'seven', 'Go Down Deh', 'Sean Paul, Shaggy', '00:02:30', '2023-05-25 21:18:16', '2023-05-25 21:18:16'),
+(8, 'eight', 'Thunder', 'Imagine Dragons', '00:00:00', '2023-05-25 21:19:46', '2023-05-25 21:19:46'),
+(9, 'nine', 'Home', 'Machine Gun Kelly, Ambassadors', '00:00:00', '2023-05-25 21:21:05', '2023-05-25 21:21:05'),
+(10, 'ten', 'Diamonds', 'Rihanna ft Josef Salvat', '00:00:00', '2023-05-25 21:21:47', '2023-05-25 21:21:47');
+
 -- --------------------------------------------------------
 
 --
--- Table structure for table `new_table`
+-- Table structure for table `playlist`
 --
 
-CREATE TABLE `new_table` (
+CREATE TABLE `playlist` (
   `id` int(11) NOT NULL,
   `name` varchar(45) DEFAULT NULL,
   `created_at` timestamp NULL DEFAULT current_timestamp(),
@@ -91,6 +108,15 @@ CREATE TABLE `users` (
   `updated_at` timestamp NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_hungarian_ci;
 
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `username`, `email`, `password`, `created_at`, `updated_at`) VALUES
+(2, 'Geri', 'g@gmail.com', '-1163669833', '2023-05-18 15:04:04', '2023-05-18 15:04:04'),
+(3, 'geri', 'gerixy20@gmail.com', '3169173', '2023-05-19 22:01:13', '2023-05-19 22:01:13'),
+(4, 'dani', 'dani@gmail.com', '3075832', '2023-05-24 18:46:17', '2023-05-24 18:46:17');
+
 -- --------------------------------------------------------
 
 --
@@ -130,9 +156,9 @@ ALTER TABLE `musics`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indexes for table `new_table`
+-- Indexes for table `playlist`
 --
-ALTER TABLE `new_table`
+ALTER TABLE `playlist`
   ADD PRIMARY KEY (`id`);
 
 --
@@ -173,7 +199,7 @@ ALTER TABLE `user_sub`
 -- AUTO_INCREMENT for table `musics`
 --
 ALTER TABLE `musics`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
 -- AUTO_INCREMENT for table `playlist_music`
@@ -191,7 +217,7 @@ ALTER TABLE `subscriptions`
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- AUTO_INCREMENT for table `user_playlist`
